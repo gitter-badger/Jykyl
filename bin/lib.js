6a2edc95260fd409cf2e778a1e0bb4f4b948f87f
serverLua <andy200511@126.com>
2018-08-29 14:29:55 +0800

    commit 2

--- bin/lib.js
+++ bin/lib.js
@@ -2,12 +2,12 @@
 var _this = this;
 exports.__esModule = true;
 var fs_1 = require("fs");
-var emitFunc;
+var fs_2 = require("fs");
 var packedObjectBoilerplate = {
     fileContnent: null,
     fileType: "",
     emit: function (command) {
-        _this.fileContnent = command();
+        _this.fileContnent = command(_this.fileContnent);
         return _this;
     }
 };
@@ -21,5 +21,14 @@ exports.pack = {
                 packedFile.fileContnent = data;
             return packedFile;
         });
+    },
+    packWrite: function (packedObject, filepath) {
+        fs_2.writeFile(filepath, packedObject.fileContnent, { mode: "w+" }, function (err) {
+            if (err) {
+                throw err;
+            }
+        });
     }
+};
+exports.processJavaScript = function (filecontnent) {
 };
